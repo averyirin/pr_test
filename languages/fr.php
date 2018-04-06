78e083fa700b481071c6f7d1a1a8740b45d04a4d
Irin Avery <averyirin@gmail.com>
2018-04-06 08:59:09 -0400

    first commit

---
+++ languages/fr.php
@@ -0,0 +1,20 @@
+<?php
+$language = array (
+	'projects' =>  'Projets',
+    /**
+     * Confluence Integration
+     */
+    'projects:confluence:add' => 'Ajouter à Confluence',
+    'projects:confluence:already' => 'Déjà à Confluence',
+    'projects:confluence:view' => 'Voir à Confluence',
+    'projects:confluence:modal:title' => "Ajouter le projet à Confluence",
+    'projects:confluence:modal:addTo' => "Ajouter à Confluence",
+    'projects:confluence:modal:x' => "x",
+    'projects:confluence:modal:newSpace' => "Nouvel espace",
+    'projects:confluence:modal:newSpaceDesc' => "(Un nouvel espace sera automatiquement créé)",
+    'projects:confluence:modal:existingSpace' => "Espace existant",
+    'projects:confluence:modal:createNewSpace' => "Créer un nouvel espace",
+    'projects:confluence:modal:addExistingSpace' => "Ajouter à l'espace existant",
+
+);
+add_translation("fr", $language);
